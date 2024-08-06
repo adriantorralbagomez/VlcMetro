109f6bd1f3a0920243a7abfdd2f6068864641893
Adrián Torralba Gómez <adriantorralba1@gmail.com>
2024-08-06 10:41:41 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        title: "VlcMetro",
+        maximizable: true,
+        width: 800,
+        height: 600,
+        minWidth: 400,
+        minHeight: 400,
+    });
+
+    window.loadFile('index.html');
+    window.webContents.openDevTools();
+};
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () =>{
+    app.quit();
+});
---
+++ renderer.js
@@ -0,0 +1,37 @@
+// //llamada de prueba av.cid
+
+// $.ajax({
+//     url: 'https://geoportal.valencia.es/geoportal-services/api/v1/salidas-metro.html?estacion=18',
+//     method: 'GET',
+//     success: function(response) {
+//         // Handle the API response here
+//         $('iframe').attr("src",response);
+//     },
+//     error: function(xhr, status, error) {
+//         // Handle errors here
+//         console.error(status, error);
+//     }
+// });
+
+// IFRAME AV CID
+
+// <iframe src="https://geoportal.valencia.es/geoportal-services/api/v1/salidas-metro.html?estacion=18" frameborder="0" style="display:block; width:100vw; height:100vh;"></iframe>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
