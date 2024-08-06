a9590e01b90f07ce0b77660df4c634de00a6c76b
Adrián Torralba Gómez <adriantorralba1@gmail.com>
2024-08-06 14:06:43 +0200

    jquery ya va

--- renderer.js
+++ renderer.js
@@ -14,35 +14,37 @@
 // });
 
 //AÑADIR ESTACIONES AL DATALIST DESPLEGABLE
-
-const stops = require('./data_json/stops.json');
-console.log($("datalist#estaciones"));
-stops.forEach(stop => {
-    var opt = document.createElement("option");
-    opt.value = stop.stop_id;
-    opt.id = stop.stop_id;
-    opt.text = stop.stop_name
-    $("datalist#estaciones").append(opt);
-});
-
-const $buscar = $("#buscar");
-
-$buscar.on("click", () => {
-    if ($("input#estaciones").value && $("input#estaciones").value != "" && $("input#estaciones").value != null && $("input#estaciones").value != 0) {
-        //hay estación
-        const est = stops.filter((stop) => {
-            return stop.stop_id == $("input#estaciones").value
-        });
-        if(est != 0 && est != null && est != ""){
-            var urlInfo = '<iframe src="https://geoportal.valencia.es/geoportal-services/api/v1/salidas-metro.html?estacion='+ $("input#estaciones").value +'" frameborder="0" style="display:block; width:100vw; height:100vh;"></iframe>';
-            $("#infoparada").html(urlInfo);
+$(document).ready(function() {
+    const stops = require('./data_json/stops.json');
+    console.log($("datalist#estaciones"));
+    
+    stops.forEach(stop => {
+        var opt = document.createElement("option");
+        opt.value = stop.stop_name;
+        opt.id = stop.stop_id;
+        opt.text = stop.stop_name;
+        $("datalist#estaciones").append(opt);
+    });
+    $("#buscar").on("click", () => {
+        if ($("input#estaciones").value && $("input#estaciones").value != "" && $("input#estaciones").value != null && $("input#estaciones").value != 0) {
+            //hay estación
+            const est = stops.filter((stop) => {
+                return stop.stop_id == $("input#estaciones").value
+            });
+            if(est != 0 && est != null && est != ""){
+                $("#titulo").html(est.stop_name);
+                var urlInfo = '<iframe src="https://geoportal.valencia.es/geoportal-services/api/v1/salidas-metro.html?estacion='+ $("input#estaciones").value +'" frameborder="0" style="display:block; width:100vw; height:100vh;"></iframe>';
+                $("#infoparada").html(urlInfo);
+            } else {
+                alert("No se ha seleccionado ninguna estación");
+            }
         } else {
             alert("No se ha seleccionado ninguna estación");
         }
-    } else {
-        alert("No se ha seleccionado ninguna estación");
-    }
+    });
+        
 });
+
 
 
 
